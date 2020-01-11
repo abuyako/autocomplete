30177af6e5c99f3fd0909350d66de79e90a8169e
abuyako <abuyako@gmail.com>
2020-01-11 13:32:24 +0300

    adding the js file

---
+++ app.js
@@ -0,0 +1,10 @@
+function autocomplete (inp, arr) {
+    var currentFocus;
+    inp.addEventListener("input", function(e)) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+
+    }
+}
